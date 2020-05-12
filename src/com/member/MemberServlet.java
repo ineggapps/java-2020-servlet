3ed872044646b837153182a65fc24b9e0eee8115
inegg <inegg.apps@gmail.com>
2020-05-12 17:29:40 +0900

    로그인/로그아웃 기능 구현

--- src/com/member/MemberServlet.java
+++ src/com/member/MemberServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/member/*")
 public class MemberServlet extends HttpServlet {
@@ -59,17 +60,53 @@ public class MemberServlet extends HttpServlet {
 
 	private void loginForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 로그인 폼
-
+		String path = "/WEB-INF/views/member/login.jsp";
+		forward(req, resp, path);
 	}
 
 	private void loginSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 로그인 처리
+		String cp = req.getContextPath();
 
+		MemberDAO dao = new MemberDAO();
+
+		String userId = req.getParameter("userId");
+		String userPwd = req.getParameter("userPwd");
+		MemberDTO dto = dao.readMember(userId);
+
+		// 로그인이 실패한 경우
+		// null check가 먼저되어야 함.
+		if (dto == null || /* !dto.getUserId().contentEquals(userId) || */ !dto.getUserPwd().equals(userPwd)
+				|| dto.getEnabled() != 1) {
+			String s = "아이디 또는 비밀번호가 일치하지 않습니다.";
+			req.setAttribute("message", s);
+			forward(req, resp, "/WEB-INF/views/member/login.jsp");
+			return;
+		}
+
+		// 로그인 성공한 경우
+		HttpSession session = req.getSession();
+		// 세션 유지시간 20분으로 변경
+		session.setMaxInactiveInterval(60 * 20);// 단위(초) 60초*20번 => 20분
+		// 세션에 로그인 정보 저장
+		SessionInfo info = new SessionInfo();
+		info.setUserId(dto.getUserId());
+		info.setUserName(dto.getUserName());
+
+		session.setAttribute("member", info);
+
+		// 첫 화면으로 이동하기
+		resp.sendRedirect(cp);
 	}
 
 	private void logout(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 로그아웃
-
+		String cp = req.getContextPath();
+		HttpSession session = req.getSession();
+		// 세션 정보를 모두 지우고 초기화
+		session.invalidate();
+		//첫 화면으로 이동
+		resp.sendRedirect(cp);
 	}
 
 	private void memberForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -92,11 +129,15 @@ public class MemberServlet extends HttpServlet {
 		dto.setBirth(req.getParameter("birth"));
 		String email1 = req.getParameter("email1");
 		String email2 = req.getParameter("email2");
-		dto.setEmail(email1 + "@" + email2);
+		if (email1.length() > 0 && email2.length() > 0) {
+			dto.setEmail(email1 + "@" + email2);
+		}
 		String tel1 = req.getParameter("tel1");
 		String tel2 = req.getParameter("tel2");
 		String tel3 = req.getParameter("tel3");
-		dto.setTel(tel1 + "-" + tel2 + "-" + tel3);
+		if (tel1.length() > 0 && tel2.length() > 0 && tel3.length() > 0) {
+			dto.setTel(tel1 + "-" + tel2 + "-" + tel3);
+		}
 		dto.setZip(req.getParameter("zip"));
 		dto.setAddr1(req.getParameter("addr1"));
 		dto.setAddr2(req.getParameter("addr2"));
@@ -105,7 +146,11 @@ public class MemberServlet extends HttpServlet {
 			dao.insertMember(dto);
 		} catch (Exception e) {
 			e.printStackTrace();
-			resp.sendRedirect(cp + "/member/member.do"); // TODO: 추후 오류처리 페이지로...?
+			String message = "회원가입이 실패했습니다.";
+			req.setAttribute("title", "회원 가입");
+			req.setAttribute("mode", "created");
+			req.setAttribute("message", message);
+			forward(req, resp, "/WEB-INF/views/member/member.jsp");
 			return;
 		}
 
